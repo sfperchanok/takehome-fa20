36cdc212effa522a7b4a73e7a04bee7d30194505
repl.it user <replituser@example.com>
2020-09-13 15:41:01 +0000

    Finish challenge

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import Instructions from './Instructions'
 import Restaurant from './Restaurant'
+import Counter from './Counter'
 
 class App extends Component {
   constructor(props) {
@@ -10,19 +11,35 @@ class App extends Component {
         {id: 1, name: "Golden Harbor", rating: 10},
         {id: 2, name: "Potbelly", rating: 6},
         {id: 3, name: "Noodles and Company", rating: 8},
-      ]
+      ],
+      inputValue: ""
     }
   }
 
   render() {
     return (
       <div className="App">
-        <Instructions />
+        <Counter count={0} />
+        <Instructions complete={true} />
         {this.state.restaurants.map(x => (
           <Restaurant id={x.id} name={x.name} rating={x.rating} />
         ))}
+        <input newName = {this.state.inputValue} onChange={evt => this.updateInput(evt)} />
+        <button type="button" onClick={this.handleInput}>input</button>
       </div>
     )
+  }
+
+  updateInput(evt) {
+    this.setState({
+      inputValue: evt.target.value
+    });
+  }
+
+  handleInput = () => {
+    this.setState((prevState) => ({
+      restaurants: [...prevState.restaurants, {id: prevState.restaurants.length, name: prevState.inputValue, rating: 0}]
+    }));
   }
 }
 
--- frontend/src/Restaurant.js
+++ frontend/src/Restaurant.js
@@ -2,11 +2,21 @@ import React, { Component } from 'react'
 import Counter from './Counter'
 
 class App extends Component {
-  // YOUR CODE GOES BELOW
+  constructor(props) {
+    super(props)
+    this.state = {
+      id: props.id,
+      name: props.name,
+      rating: props.rating
+    }
+  }
   
   render() {
     return (
-      <div/> 
+      <div>
+        {this.state.name}
+        <Counter count={this.state.rating} />
+      </div>
     )
   }
 }
